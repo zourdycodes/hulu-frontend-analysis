ac19737f2bbf2f5034c1c54e34fcb38490a8221b
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-17 14:22:23 +0700

    :triangular_flag_on_post: in-making of header component -> rendering the sub-component
    navigation icon for the left side - on-going

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 
+import { HeaderItem } from '../HeaderItem';
+import { navigationData } from '../../devData/navigation';
+
 export const Header = () => {
   return (
     <header>
+      <div>
+        {navigationData.map((item, index) => {
+          return <HeaderItem key={index} title={item.title} Icon={item.icon} />;
+        })}
+      </div>
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
---
+++ devData/navigation.js
@@ -0,0 +1,35 @@
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+
+export const navigationData = [
+  {
+    title: 'HOME',
+    icon: HomeIcon,
+  },
+  {
+    title: 'TRENDING',
+    icon: LightningBoltIcon,
+  },
+  {
+    title: 'VERIFIED',
+    icon: BadgeCheckIcon,
+  },
+  {
+    title: 'COLLECTIONS',
+    icon: CollectionIcon,
+  },
+  {
+    title: 'SEARCH',
+    icon: SearchIcon,
+  },
+  {
+    title: 'ACCOUNT',
+    icon: UserIcon,
+  },
+];
