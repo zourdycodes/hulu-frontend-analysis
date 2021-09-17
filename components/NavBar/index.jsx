f893a4367727d2d20176d9d968893b4e00b8d20a
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-17 15:55:08 +0700

    :pushpin: done building the tag featured components
    checked

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 import { fetchHelpers } from '../../utils/requests';
 
 export const NavBar = () => {
+  const router = useRouter();
+
   return (
-    <nav>
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide ">
         {Object.entries(fetchHelpers).map(([key, { title }]) => (
           <h2
+            onClick={() => router.push(`/?genre=${key}`)}
             key={key}
-            className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
           </h2>
         ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202a] h-10 w-1/12 " />
     </nav>
   );
 };
