d66bf012a2cda75eb22cba66942812996d4a3e43
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-17 15:32:20 +0700

    :fire: almost done building the featured movies tags components
    checked

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { Header } from './Header';
+export { NavBar } from './NavBar';
 export { HeaderItem } from './HeaderItem';
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 
-import { Header } from '../components';
+import { Header, NavBar } from '../components';
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <NavBar />
     </div>
   );
 }
