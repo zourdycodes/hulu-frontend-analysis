09e2c953951687048618328b735ba9f5ad105540
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-17 23:06:21 +0700

    :rocket: finished and readty to abroad
    checked woooozze

--- components/Results/index.jsx
+++ components/Results/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import FlipMove from 'react-flip-move';
 
 import { MovieCard } from '../MovieCard';
 
 export const Results = ({ moviesData }) => {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4  3xl:flex flex-wrap justify-center">
       {moviesData?.map((result) => {
         return <MovieCard key={result.id} {...result} />;
       })}
-    </div>
+    </FlipMove>
   );
 };
